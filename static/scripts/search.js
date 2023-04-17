47b98dd21e0fe92f674480a9b7285f26666dee85
fractalenjoyer <92572174+fractalenjoyer@users.noreply.github.com>
2023-04-17 10:18:15 +0200

    moved stuff around

--- static/scripts/search.js
+++ static/scripts/search.js
@@ -10,3 +10,9 @@ const showGame = (id) => {
 modal.onclick = (e) => {
 	e.target.style.display = "none";
 };
+
+document.querySelector("#search").onkeyup = (e) => {
+	if (e.key === "Enter") {
+		window.location.href = `/search/${e.target.value}`;
+	}
+}
